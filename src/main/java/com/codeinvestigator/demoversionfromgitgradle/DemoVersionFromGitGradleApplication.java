e52389217cb92405a5673b009112ddf86d209bfe
Mike Møller Nielsen <mmni@protonmail.com>
2019-08-13 22:18:19 +0200

    First commit

---
+++ src/main/java/com/codeinvestigator/demoversionfromgitgradle/DemoVersionFromGitGradleApplication.java
@@ -0,0 +1,13 @@
+package com.codeinvestigator.demoversionfromgitgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoVersionFromGitGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoVersionFromGitGradleApplication.class, args);
+    }
+
+}
